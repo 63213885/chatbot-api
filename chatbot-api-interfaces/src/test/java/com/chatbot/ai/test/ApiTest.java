56e52195db4dd1961932797708682b7270ec621f
jzz <63213885@qq.com>
2025-11-11 18:10:31 +0800

    init project

---
+++ chatbot-api-interfaces/src/test/java/com/chatbot/ai/test/ApiTest.java
@@ -0,0 +1,13 @@
+package com.chatbot.ai.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 单元测试
+ */
+@SpringBootApplication
+public class ApiTest {
+
+
+}
